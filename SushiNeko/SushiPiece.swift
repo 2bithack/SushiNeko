19ed1fb2e33fee70692584e8d2478381872dad19
enzobotter <enzobot@hotmail.com>
2016-06-28 15:05:47 -0400

    susvhineko setup
    just beginning to set up sushi neko

---
+++ SushiNeko/SushiPiece.swift
@@ -0,0 +1,34 @@
+//
+//  SushiPiece.swift
+//  SushiNeko
+//
+//  Created by enzo bot on 6/28/16.
+//  Copyright © 2016 GarbageGames. All rights reserved.
+//
+
+import SpriteKit
+
+class SushiPiece: SKSpriteNode {
+    
+    /* Chopsticks objects */
+    var rightChopstick: SKSpriteNode!
+    var leftChopstick: SKSpriteNode!
+    
+    /* You are required to implement this for your subclass to work */
+    override init(texture: SKTexture?, color: UIColor, size: CGSize) {
+        super.init(texture: texture, color: color, size: size)
+    }
+    
+    /* You are required to implement this for your subclass to work */
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func connectChopsticks() {
+        /* Connect the child chopstick nodes */
+        
+        rightChopstick = childNodeWithName("rightChopstick") as! SKSpriteNode
+        leftChopstick = childNodeWithName("leftChopstick") as! SKSpriteNode
+    }
+    
+}
